a83118c174735e5cae0cdc24f93afe93f61cf094
Stephen Fritsen <smfritsen@proton.me>
2024-11-03 22:28:03 -0700

    Slime enemy added

---
+++ scripts/enemy.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+var speed = 40
+var player_chase = false
+var player = null
+
+func _physics_process(delta: float) -> void:
+	if player_chase:
+		position += (player.position - position)/speed
+		
+		# Play walk when player is in range
+		$AnimatedSprite2D.play("walk")
+		
+		# Flip enemy based on player direction
+		if(player.position.x - position.x) < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+	else:
+		# Play idle when out of range
+		$AnimatedSprite2D.play("idle")
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	player = body
+	player_chase = true
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	player = null
+	player_chase = false
