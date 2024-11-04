9833d168d7b36eabb2f3b5f151d726db50f7d741
Stephen Fritsen <smfritsen@proton.me>
2024-11-03 23:13:31 -0700

    Enemy and player combat

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,8 +3,13 @@ extends CharacterBody2D
 var speed = 40
 var player_chase = false
 var player = null
+var health = 100
+var player_inattack_zone = false
+var can_take_damage = true
 
 func _physics_process(delta: float) -> void:
+	deal_with_damage()
+
 	if player_chase:
 		position += (player.position - position)/speed
 		
@@ -31,3 +36,27 @@ func _on_detection_area_body_exited(body: Node2D) -> void:
 # This is a method used by other scripts to see if its an enemy
 func enemy():
 	pass
+
+
+func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = true
+
+
+func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = false
+
+func deal_with_damage():
+	if player_inattack_zone and global.player_current_attack == true:
+		if can_take_damage == true:
+			health = health - 20
+			$take_damage_cooldown.start() # call timer to start dmg cooldown
+			can_take_damage = false
+			print("Slime Health: ", health)
+			if health <= 0:
+				self.queue_free()
+
+
+func _on_take_damage_cooldown_timeout() -> void:
+	can_take_damage = true
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,8 +2,9 @@ extends CharacterBody2D
 
 var enemy_inattack_range = false
 var enemy_attack_cooldown = true
-var health = 100
+var health = 200
 var player_alive = true
+var attack_ip = false # ip = in progress
 
 const speed = 100
 var current_dir = "none"
@@ -15,6 +16,7 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	player_movement(delta)
 	enemy_attack()
+	attack()
 	
 	if health <= 0:
 		player_alive = false # add death or respawn screen here
@@ -59,27 +61,31 @@ func play_anim(movement):
 		if movement == 1:
 			anim.play("side_walk")
 		elif movement == 0:
-			anim.play("side_idle")
+			if attack_ip == false:
+				anim.play("side_idle")
 	if dir == "left":
 		anim.flip_h = true
 		if movement == 1:
 			anim.play("side_walk")
 		elif movement == 0:
-			anim.play("side_idle")
+			if attack_ip == false:
+				anim.play("side_idle")
 	if dir == "down":
 		anim.flip_h = true
 		if movement == 1:
 			anim.play("front_walk")
 		elif movement == 0:
-			anim.play("front_idle")
+			if attack_ip == false:
+				anim.play("front_idle")
 	if dir == "up":
 		anim.flip_h = true
 		if movement == 1:
 			anim.play("back_walk")
 		elif movement == 0:
-			anim.play("back_idle")
+			if attack_ip == false:
+				anim.play("back_idle")
 
-# This is a method used by other scripts to see if its an player
+# This is a method used by other scripts to see if its a player
 func player():
 	pass
 
@@ -101,8 +107,33 @@ func enemy_attack():
 		# Set cooldown to false
 		enemy_attack_cooldown = false
 		$attack_cooldown.start()
-		print(health)
-
+		print("Player Health: ", health)
 
 func _on_attack_cooldown_timeout() -> void:
 	enemy_attack_cooldown = true
+	
+func attack():
+	var dir = current_dir
+	
+	if Input.is_action_just_pressed("attack"):
+		global.player_current_attack = true
+		attack_ip = true
+		if dir == "right":
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.play("side_attack")
+			$deal_attack_timer.start()
+		if dir == "left":
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("side_attack")
+			$deal_attack_timer.start()
+		if dir == "down":
+			$AnimatedSprite2D.play("front_attack")
+			$deal_attack_timer.start()
+		if dir == "up":
+			$AnimatedSprite2D.play("back_attack")
+			$deal_attack_timer.start()
+
+func _on_deal_attack_timer_timeout() -> void:
+	$deal_attack_timer.stop()
+	global.player_current_attack = false
+	attack_ip = false
